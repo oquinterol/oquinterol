b3141e2afa7149e67baa21ebcc908fc45e08525e
Alexis Quintero <oquinterol@unal.edu.co>
2024-04-02 16:23:09 -0500

    Add site link

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.me',
+	site: 'https://quinterol.com',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
